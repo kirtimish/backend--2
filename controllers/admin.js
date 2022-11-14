89496587a357d1ae296c755fb01cf503529b5330
kirtimish <kirtimishra0708@gmail.com>
2022-11-14 08:08:45 +0530

    UsingAssociations

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,14 +13,15 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user.createProduct({
     title: title,
     price: price,
     imageUrl: imageUrl,
     description: description
   })
   .then(result => {
-    console.log(result);
+    // console.log(result);
+    console.log('created product');
     res.redirect('/products');
   })
   .catch(err => {
@@ -34,8 +35,10 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-  .then(product => {
+  req.user.getProducts({ where : {id: prodId}})
+  // Product.findByPk(prodId)
+  .then(products => {
+    const product = products[0];
     if (!product) {
       return res.redirect('/');
     }
@@ -71,7 +74,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  // Product.findAll()
+  req.user.getProducts()
   .then((products) =>{
     res.render('admin/products', {
       prods: products,
