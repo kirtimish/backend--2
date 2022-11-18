f5491a79ef984091839d134321d67b08b4eeae15
kirtimish <kirtimishra0708@gmail.com>
2022-11-19 04:45:35 +0530

    integraion of products from BE to FE

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+var cors = require('cors');
 
 const errorController = require('./controllers/error');
 const db = require('./util/database');
@@ -21,6 +22,7 @@ const CartItem = require('./models/cartItem');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use((req,res,next) => {
     User.findByPk(1)
